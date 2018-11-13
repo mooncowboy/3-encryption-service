628aed69137516ada9c92fd59ca27aee38bc186a
Artem Mitrokhin <miartem@microsoft.com>
2018-11-13 10:33:43 +0100

    Add Utilities and Utilities.Tests projects

---
+++ Encryption.Utilities.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Encryption.Utilities.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
